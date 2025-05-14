6ec1ddd8898e2b1ca0c3f3fc9d4b66d18be9e600
pashathelooser <84590491+pashathelooser@users.noreply.github.com>
2025-05-15 00:43:05 +0500

    Вложенные папки работают, но без TFIDF

--- Foogle.py
+++ Foogle.py
@@ -6,11 +6,11 @@ from collections import defaultdict
 
 class Foogle:
     def __init__(self, directory):
-        self.directory = directory
+        self.start_directory = directory
         self.index = defaultdict(lambda: defaultdict(int))  # Вложенный словарь {word: {filename: count}}
         self.documents = []
         self.idf = {}
-        self.build_index()
+        self.build_index(directory)
         self.calculate_idf()
 
     def clean_text(self, text):
@@ -22,67 +22,69 @@ class Foogle:
         """Разбивает на отдельные слова по пробелу"""
         return text.split()
 
-    def read_file(self, filename):
+    def read_file(self, filename, dir):
         """Возвращает содержимое файла в виде строки"""
         try:
-            file = open(os.path.join(self.directory, filename), 'r')
+            file = open(os.path.join(dir, filename), 'r')
             return file.read()
         except Exception as e:
             print(f"Ошибка чтения файла {filename}: {e}")
             return ""
 
-    def build_index(self):
+    def build_index(self, directory_name):
         """Создает индекс слов для каждого файла в директории."""
-        print("Индекс файл собирается...")
-        for filename in os.listdir(self.directory):
+        for filename in os.listdir(directory_name):
             if not filename.endswith(".txt"):
+                possible_directory = directory_name + "/" + filename
+                if os.path.isdir(possible_directory):
+                    self.build_index(possible_directory)
                 continue
-            self.documents.append(filename)
-            text = self.read_file(filename)
+            self.documents.append(filename
+                                  if directory_name == self.start_directory
+                                  else directory_name + "/" + filename)
+            text = self.read_file(filename, directory_name)
             if not text:
                 continue
             cleaned_text = self.clean_text(text)
             tokens = self.tokenize(cleaned_text)
             for term in tokens:
                 self.index[term][filename] += 1
-        print("Индекс файл готов.")
 
     def calculate_idf(self):
         """Вычисляет IDF для каждого слова."""
-        print("Calculating IDF...")
+        print("Вычисляется IDF...")
         N = len(self.documents)
         for term in self.index:
             df = len(self.index[term])
             self.idf[term] = math.log(N / df)
-        print("IDF calculated.")
+        print("IDF вычислен")
 
-    def tf(self, term, document):
+    def get_tf(self, term, document):
         """Вычисляет TF для заданного слова в документе."""
         if term in self.index and document in self.index[term]:
-            # Raw term frequency
             return self.index[term][document]
-        else:
-            return 0
+        return 0
 
     def tf_idf(self, term, document):
         """Вычисляет TF-IDF для слова в документе."""
-        return self.tf(term, document) * self.idf.get(term, 0)
+        return self.get_tf(term, document) * self.idf.get(term, 0)
 
     def search(self, query):
-        """Ищет документы, содержащие запрос, и сортирует их по TF-IDF."""
+        """Ищет документы, содержащие запрос и сортирует их по TF-IDF."""
         cleaned_query = self.clean_text(query)
         query_terms = self.tokenize(cleaned_query)
 
-        document_scores = defaultdict(float)
+        document_score = defaultdict(float)
         for document in self.documents:
             for term in query_terms:
-                document_scores[document] += self.tf_idf(term, document)
+                document_score[document] += self.tf_idf(term, document)
 
-        ranked_results = sorted(document_scores.items(), key=lambda item: item[1], reverse=True)
-        return ranked_results
+        result = sorted(document_score.items(), key=lambda item: item[1], reverse=True)
+        return result
 
 
-directory = "search_files"
+print("Введите директорию для поиска:")
+directory = input()
 if not os.path.exists(directory):
     print(f"Директория {directory} не существует")
 
