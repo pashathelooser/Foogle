11f6f69a5ee39a77785234d20dcb9f499fc79bc0
pashathelooser <pavelvatutin08@gmail.com>
2025-05-31 23:56:44 +0500

    index and docs storage ready

--- Foogle.py
+++ Foogle.py
@@ -1,3 +1,5 @@
+import hashlib
+import json
 import os
 import math
 import string
@@ -8,9 +10,9 @@ class Foogle:
     def __init__(self, directory):
         self.start_directory = directory
         self.index = defaultdict(lambda: defaultdict(int))  # Вложенный словарь {word: {filename: count}}
-        self.documents = []
+        self.documents = defaultdict(str)
         self.idf = {}
-        self.build_index(directory)
+        self.check_if_update_needed()
         self.calculate_idf()
 
     def clean_text(self, text):
@@ -31,27 +33,98 @@ class Foogle:
             print(f"Ошибка чтения файла {filename}: {e}")
             return ""
 
+    def check_docs_hash(self, old_docs):
+        """Проверяет изменились ли hash значения у файлов"""
+        for document in old_docs:
+            file = open(document).read()
+            if self.hash_txt(file) != old_docs[document]:
+                return False
+        return True
+
+    def check_if_update_needed(self):
+        """Проверяет index и docs на соответствие файлам текущей директории"""
+        old_index = self.read_index()
+        old_docs = self.read_docs()
+        if not old_index:
+            self.build_index(self.start_directory)
+        elif old_index["working directory"]["current"] == self.start_directory and self.check_docs_hash(old_docs):
+            self.index = old_index
+            self.documents = old_docs
+        else:
+            self.build_index(self.start_directory)
+
     def build_index(self, directory_name):
         """Создает индекс слов для каждого файла в директории."""
+        self.index["working directory"]["current"] = self.start_directory
         for filename in os.listdir(directory_name):
             if not filename.endswith(".txt"):
                 possible_directory = os.path.join(directory_name, filename)
                 if os.path.isdir(possible_directory):
                     self.build_index(possible_directory)
                 continue
-            self.documents.append(os.path.join(directory_name, filename))
             text = self.read_file(filename, directory_name)
+            self.documents[os.path.join(directory_name, filename)] = self.hash_txt(text)
             if not text:
                 continue
             cleaned_text = self.clean_text(text)
             tokens = self.tokenize(cleaned_text)
             for term in tokens:
                 self.index[term][os.path.abspath(os.path.join(directory_name, filename))] += 1
+        self.write_docs()
+        self.write_index()
+
+    def hash_txt(self, text):
+        """Хэширует строку"""
+        hash_obj = hashlib.new("sha256")
+        hash_obj.update(text.encode('utf-8'))
+        return hash_obj.hexdigest()
+
+    def write_docs(self):
+        """Записывает documents в docs.json"""
+        with open("docs.json", 'w') as file:
+            json.dump(self.convert_to_dict(self.documents), file, indent=4)
+
+    def read_docs(self):
+        """Возвращает содержимое docs.json"""
+        with open("docs.json") as file:
+            c = file.read().strip()
+            if not c:
+                return False
+        with open("docs.json") as file:
+            d = json.load(file)
+            return self.convert_to_defaultdict(d)
+
+    def write_index(self):
+        """Записывает index в index.json"""
+        with open("index.json", 'w') as file:
+            json.dump(self.convert_to_dict(self.index), file, indent=4)
+
+    def convert_to_defaultdict(self, d):
+        if isinstance(d, dict):
+            return defaultdict(int, {k: self.convert_to_defaultdict(v) for k, v in d.items()})
+        return d
+
+    def convert_to_dict(self, d):
+        if isinstance(d, defaultdict):
+            return {k: self.convert_to_dict(v) for k, v in d.items()}
+        return d
+
+    def read_index(self):
+        """Возвращает содержимое index.json"""
+        with open("index.json") as file:
+            c = file.read().strip()
+            if not c:
+                return False
+        with open("index.json", 'r') as file:
+            d = json.load(file)
+            return self.convert_to_dict(d)
 
     def calculate_idf(self):
         """Вычисляет IDF для каждого слова."""
         N = len(self.documents)
         for term in self.index:
+            if term == "working directory":
+                continue
             df = len(self.index[term])
             self.idf[term] = math.log(N / df)
 
@@ -142,7 +215,10 @@ while True:
     try:
         command = input(f"[{current_directory}]> ")
 
-        if command == "exit":
+        if not command:
+            print("type something")
+
+        elif command == "exit":
             print("Exiting application")
             break
 
@@ -152,7 +228,7 @@ while True:
                 current_directory = new_directory
                 engine = Foogle(current_directory)
 
-        elif command.startswith("search"):
+        elif command.split()[0].startswith("search"):
             if len(command.split()) == 1:
                 print("Type what you want to search")
                 continue
